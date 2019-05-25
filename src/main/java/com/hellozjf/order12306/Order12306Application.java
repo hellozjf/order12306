888be0e6ff3f5a1232d50067140e93bce0fd8bb4
hellozjf <908686171@qq.com>
2019-05-25 16:43:58 +0800

    init

---
+++ src/main/java/com/hellozjf/order12306/Order12306Application.java
@@ -0,0 +1,13 @@
+package com.hellozjf.order12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order12306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Order12306Application.class, args);
+    }
+
+}
